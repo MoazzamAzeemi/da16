f2c81b25c0b158c9b13e2ac683c6a39ae62f4054
MoazzamAzeemi <Moazzam.Azeemi@gmail.com>
2023-02-11 14:32:40 +0500

    First Commit from PyCharm

---
+++ main.py
@@ -0,0 +1,46 @@
+## Coffee machine
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electrical", "Water", "Fire"])
+# table.align = "l"
+# print(table)
+
+
+# import turtle
+# mano = turtle.Turtle()
+# mano.shape("turtle")
+# mano.color("blue")
+# mano.turtlesize(stretch_wid=5, stretch_len=5, outline=0.5)
+# print(mano)
+# mano.forward(200)
+# my_screen = turtle.Screen()
+# my_screen.bgcolor("red")
+#
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
+
+# # This is a sample Python script.
+#
+# # Press Ctrl+F5 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
