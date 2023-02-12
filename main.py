d735cb47f3894c532ad46d5c5c07ce301d4b0c7e
MoazzamAzeemi <Moazzam.Azeemi@gmail.com>
2023-02-12 21:10:21 +0500

    Day 16 completed

--- main.py
+++ main.py
@@ -2,7 +2,46 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+## Coffee Machine
 
+# money = 0
+machine_on = True
+#
+
+my_money_machine = MoneyMachine()
+my_coffee_maker = CoffeeMaker()
+drink = Menu()
+
+# print report about resources left
+while machine_on:
+    options = drink.get_items()
+    flavour = input(f"What would you like? {options}: ").lower()
+    if flavour=='off':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        machine_on = False
+    elif flavour == 'report':
+        my_coffee_maker.report()
+        my_money_machine.report()
+    elif flavour in options:
+        # check resources are sufficient
+        choice = drink.find_drink(flavour)
+        if my_coffee_maker.is_resource_sufficient(choice):
+            if my_money_machine.make_payment(choice.cost):
+                my_coffee_maker.make_coffee(choice)
+
+
+            # my_money_machine.process_coins()
+            # if cost(flavour):
+            #     print(f"Here is your {flavour}\u2615 ENJOY!")
+            # else:
+            #     print("you have paid less amount, please take it back...")
+        # else:
+        #     pass
+        #     # machine_on = False
+    else:
+        print("Please enter correct choice...")
 
 
 
